3a7092396f701003bb8f744e75df2760a6cf35c4
Cl3arglass <haltaffer@gmail.com>
2018-11-07 14:22:02 -0800

    generate static controller and add welcome message to view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
   get 'users/new'
   get 'users/create'
   get 'users/edit'
   get 'users/update'
   get 'users/show'
+
+  root to: "static#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
