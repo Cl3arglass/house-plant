60f2c8ac77d68278679d20c29ed06cef163025e9
Cl3arglass <haltaffer@gmail.com>
2018-11-12 14:25:32 -0800

    added create method for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	@user = User.find_by(name: params[:user][:name])
+	    session[:user_id] = @user.id
+	    redirect_to user_path(@user)
   end
 
   def destroy
