6dcb33be5e6ded063c78ee65832ab9b856b42426
Cl3arglass <haltaffer@gmail.com>
2018-11-13 17:13:23 -0800

    add password validation on login with flash notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,13 @@ class SessionsController < ApplicationController
 
   def create
   	@user = User.find_by(name: params[:user][:name])
-	    session[:user_id] = @user.id
-	    redirect_to user_path(@user)
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user) 
+    else 
+      flash[:notice] = "Try Again"
+      render 'sessions/new'
+    end
   end
 
   def destroy
