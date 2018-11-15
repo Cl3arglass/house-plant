9aea3c57ce428ad159996082c0eb689d9c7bcc9f
Cl3arglass <haltaffer@gmail.com>
2018-11-14 17:14:40 -0800

    add omniauth gem and secret keys with protection

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module HousePlant
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config_files = ['secrets.yml']
+
+    config_files.each do |file_name|
+      file_path = File.join(Rails.root, 'config', file_name)
+      config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+      config_keys.each do |k,v|
+        ENV[k.upcase] ||= v
+      end
+    end
   end
 end
+
+
