f1f090ee01cf99a85ee34ea9bc60dfc6bf56e419
Cl3arglass <haltaffer@gmail.com>
2018-11-19 14:34:20 -0800

    add github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,31 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user) 
-    else 
-      flash[:notice] = "Try Again"
-      render 'sessions/new'
-    end
+  	if auth_hash = request.env["omniauth.auth"]
+      oauth_email = request.env["omniauth.auth"]["info"]["email"]
+      if @user = User.find_by(:email => oauth_email)
+      	session[:user_id] = @user.id
+      	redirect_to user_path(@user) 
+      else
+      	@user = User.new(:email => oauth_email, :name => request.env["omniauth.auth"]["info"]["name"], :password => SecureRandom.hex)
+      	if @user.save
+      	  session[:user_id] = @user.id
+	      redirect_to user_path(@user) 
+	    else
+	      flash[:notice] = "Try Again"
+	      render 'sessions/new'
+	    end
+	  end
+  	else
+	  @user = User.find_by(name: params[:user][:name])
+	   if @user && @user.authenticate(params[:password])
+	      session[:user_id] = @user.id
+	      redirect_to user_path(@user) 
+	   else 
+	      flash[:notice] = "Try Again"
+	      render 'sessions/new'
+	   end
+	end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   post '/sessions' => "sessions#create"
   get '/logout' => "sessions#destroy"
   post '/rides/new' => "rides#new" 
+  get '/auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
