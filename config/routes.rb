a76400daaea21b67042bb3873f5ec2e15371ffca
Cl3arglass <haltaffer@gmail.com>
2018-11-26 12:15:23 -0800

    add nested resources for plants

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   
-  resources :users, :only => [:new, :create, :edit, :update, :show]
-  
+  resources :users do
+  # , :only => [:new, :create, :edit, :update, :show]
+    resources :plants
+  end
   
 
   root to: "static#welcome"
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   get 'signin' => "sessions#new"
   post '/sessions' => "sessions#create"
   get '/logout' => "sessions#destroy"
-  post '/rides/new' => "rides#new" 
+  # post '/rides/new' => "rides#new" 
   get '/auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
