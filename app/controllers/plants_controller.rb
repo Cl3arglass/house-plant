6e7ebe601715e11e4735015c82d055fd3ea2919f
Cl3arglass <haltaffer@gmail.com>
2018-11-28 17:11:01 -0800

    update plants params to accept comment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,8 +1,18 @@
 class PlantsController < ApplicationController
+  before_action :set_user_by_params
+
   def new
+    @plant = Plant.new
   end
 
   def create
+    # binding.pry
+    @plant = @user.plants.build(plant_params)
+    if user_valid? && @plant.save
+      redirect_to user_plants_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -19,4 +29,24 @@ class PlantsController < ApplicationController
 
   def show
   end
+
+  private
+  
+
+   def plant_params
+    params.require(:plant).permit(:name, :light, :watering, :user_id, comment_new: [:note])
+   end
+
+   def set_user_by_params
+       @user = User.find_by_id(params[:user_id]) if (params[:user_id] && current_user.id == params[:user_id].to_i)
+    if @user.nil?
+       redirect_to user_path(current_user)
+    end
+   end
+
+   def user_valid?
+    @user && @user.id == current_user.id
+  end
+
+  
 end
