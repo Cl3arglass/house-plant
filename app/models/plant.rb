5dbf2e23ab0c60c1dbded79c9546abd4ce540bee
Cl3arglass <haltaffer@gmail.com>
2018-11-28 17:12:24 -0800

    added comment new method to help great comments

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,14 @@
 class Plant < ApplicationRecord
 
 	 belongs_to :user
-     belongs_to :category
+     belongs_to :comment
+
+def comment_new=(new_comment)
+    empty = new_comment[:note].nil? || new_comment[:note].blank?
+    if !empty
+      comment = Comment.find_or_create_by(note: new_comment[:note])
+      self.comment = comment
+    end
+  end
+
 end
