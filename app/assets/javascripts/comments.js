c1c5daed69497077d5994c9396abdb1a338a7b93
Cl3arglass <haltaffer@gmail.com>
2019-04-23 16:37:51 -0700

    add view all comments link sorted by name asc

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -16,3 +16,36 @@ $(document).ready(function() {
 	e.preventDefault();
   })
 })
+
+$(document).ready(function() {
+	$("a.comments_view_sorted").on("click", function(e) {
+	
+
+	$.getJSON(this.href).success(function(json) {
+	  var $ol = $("div.comments ol");
+	  $ol.html("");
+
+	  json.sort(function(a, b) {
+		  var nameA = (a.note) ? a.note.toUpperCase() : ""; 
+		  var nameB = (b.note) ? b.note.toUpperCase() : ""; 
+		  if (nameA < nameB) {
+		    return -1;
+		  }
+		  if (nameA > nameB) {
+		    return 1;
+	      }
+
+ 
+		  return 0;
+	  });
+
+	  json.forEach(function(comment){
+	  	if (comment.note) {
+	  	  $ol.append("<li>" + "*"+ comment.note + "</li>");
+	  	}
+	  })
+	  
+	})
+	e.preventDefault();
+  })
+})
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
 
 	has_many :plants
     has_many :users, through: :plants
+
+
 end
